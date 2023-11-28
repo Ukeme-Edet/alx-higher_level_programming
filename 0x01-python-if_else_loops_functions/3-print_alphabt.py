05bf79a0e79144cf70d92ecd5b4183396bca91d6
Ukeme Edet <ukemeedet2207@gmail.com>
2023-11-28 19:43:33 +0100

    Setup linelength formatting

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 print(
-    "".join([chr(_) for _ in range(97, 123) if _ != 101 and _ != 113]).format(""),
+    "".join([chr(_) for _ in range(97, 123) if _ != 101 and _ != 113]).format(
+        ""
+    ),
     end="",
 )
