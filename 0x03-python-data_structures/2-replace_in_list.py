db0096b234a50e73552281b479e1516164db1045
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-04 13:07:02 +0100

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    return (
-        my_list[:idx] + [element] + my_list[idx + 1:]
-        if idx > -1 and idx < len(my_list)
-        else my_list
-    )
+    if idx > -1 and idx < len(my_list):
+        my_list[idx] = element
+    return my_list
