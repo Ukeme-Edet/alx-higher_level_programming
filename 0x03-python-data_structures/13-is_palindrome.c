e6ae772572cb3f75571d3b4e8645fbb48c18ff3e
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-04 19:14:59 +0100

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,27 +5,31 @@
  * @head: pointer to head of list
  * Return: 1 if palindrome, 0 if not
  */
-int is_palindrome(listint_t **head) {
-  listint_t *curr = *head;
-  long long len = 0;
-  int *arr = NULL;
+int is_palindrome(listint_t **head)
+{
+	listint_t *curr = *head;
+	long long len = 0;
+	int *arr = NULL;
 
-  if (!head || !*head)
-    return (1);
-  while (curr) {
-    curr = curr->next;
-    len++;
-  }
-  while (!arr)
-    arr = malloc(sizeof(int) * len);
-  curr = *head;
-  for (int i = 0; i < len; i++) {
-    arr[i] = curr->n;
-    curr = curr->next;
-  }
-  for (int i = 0; i < len / 2; i++) {
-    if (arr[i] != arr[len - i - 1])
-      return (0);
-  }
-  return (1);
+	if (!head || !*head)
+		return (1);
+	while (curr)
+	{
+		curr = curr->next;
+		len++;
+	}
+	while (!arr)
+		arr = malloc(sizeof(int) * len);
+	curr = *head;
+	for (int i = 0; i < len; i++)
+	{
+		arr[i] = curr->n;
+		curr = curr->next;
+	}
+	for (int i = 0; i < len / 2; i++)
+	{
+		if (arr[i] != arr[len - i - 1])
+			return (0);
+	}
+	return (1);
 }
