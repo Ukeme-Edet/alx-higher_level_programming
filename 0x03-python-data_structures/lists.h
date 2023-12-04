de940e4622d5e26d76f30386fa81901a8dd38818
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-04 19:06:01 +0100

    Linked list palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,10 +9,9 @@
  * Description: singly linked list node structure
  * for project
  */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
+typedef struct listint_s {
+  int n;
+  struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
