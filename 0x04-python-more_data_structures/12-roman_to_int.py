637ff93a925e2ddd32bd1028c402463446c52120
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-07 00:58:18 +0100

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     i = 0
     vs = {"I": 1, "V": 5, "X": 10, "C": 100, "D": 500, "M": 1000}
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     for j in range(len(roman_string)):
         if j > 0 and vs[roman_string[j]] > vs[roman_string[j - 1]]:
             i += vs[roman_string[j]] - 2 * vs[roman_string[j - 1]]
