7e7936f7c51bc5d899dbb77cd5a162be3d2d4fef
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-08 17:25:24 +0100

    Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {a_dictionary[key] * 2 for key in a_dictionary.keys()}
+    return {key: a_dictionary[key] * 2 for key in a_dictionary.keys()}
