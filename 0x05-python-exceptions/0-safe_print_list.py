48001b87787e432ea58af48b0d837afad7eb5a25
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-18 17:56:39 +0100

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
         except IndexError:
+            print()
             return i
         i += 1
         x -= 1
+    print()
+    return i
