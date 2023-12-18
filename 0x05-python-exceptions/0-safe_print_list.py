890cf6b87760058ee9c1f9ee3fa67deb61c300e4
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-18 17:54:02 +0100

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while x:
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            return i
+        i += 1
+        x -= 1
