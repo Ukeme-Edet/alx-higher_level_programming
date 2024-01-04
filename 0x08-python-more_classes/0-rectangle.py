ed57752beeaa6c69fbf71889706d3349dd66622b
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-05 00:09:11 +0100

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module that defines a class named Rectangle."""
+
+
+class Rectangle:
+    """Defines a Rectangle, some attributes and functions for Rectangles."""
+    pass
