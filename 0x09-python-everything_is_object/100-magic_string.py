2cb878cb8864670ca30ac990ff22c12509e560ab
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-05 23:21:41 +0100

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-A function that returns a string n times the number of times it has been called
-"""
-
-
 def magic_string():
-    """
-    Returns a string n times the number of times it has been called
-    """
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return ", ".join(["Holberton"] * magic_string.count)
