7156d853cc924855de5f23222bc29513e6633b60
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-05 23:19:36 +0100

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -7,5 +7,5 @@ A function that returns a string n times the number of the iteration
 def magic_string(n=0):
     """
     Returns a string n times the number of the iteration
-        """
+    """
     return ", ".join(["Holberton"] * n)
