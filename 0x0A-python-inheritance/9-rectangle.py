3c90f3af6ce4eb4c53e228138c609675a9f5e436
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-08 23:31:49 +0100

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-A module that contains the Rectangle class inheriting from BaseGeometry
+A module that defines the Rectangle class that inherits from BaseGeometry
 - Instantiation with `width` and `height`: `def __init__(self, width, height):`
     - `width` and `height` must be private. No getter or setter
     - `width` and `height` must be positive integers, validated by
     `integer_validator`
+- the `area()` method implemented
+- `print()` should print, and `str()` should return, the following
+rectangle description: `[Rectangle] <width>/<height>`
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
@@ -22,3 +25,15 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns the string representation of the rectangle
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
