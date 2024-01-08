0b075e0e3911982022d678918dd835c6519da5a3
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-08 22:18:39 +0100

    Improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module that contains a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry
+    """
+
+    def area(self):
+        """
+        Calculates the area of a geometric shape
+        """
+        raise Exception("area() is not implemented")
