3204648a29961fe14cc905821134f22a062f78fb
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-08 21:50:15 +0100

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A class that inherits from list
+"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
