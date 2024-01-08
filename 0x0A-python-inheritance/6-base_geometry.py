027d0631d55511ccc2bc90b40f2c9258ec59d669
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-08 22:23:09 +0100

    Integer validator

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 A module that contains a class BaseGeometry
+- Public instance method: `def area(self):` that raises an `Exception`
+with the message `area() is not implemented`
 """
 
 
