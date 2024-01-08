4f1f5f87904da59e01b040a3e6e667e6f23d45bc
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-08 23:16:11 +0100

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A module that contains the Rectangle class inheriting from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class Rectangle that inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializer function that validates the values of width and height
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+8-rectangle.py
