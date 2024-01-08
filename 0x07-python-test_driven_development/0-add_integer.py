e049e96056a35957c381cd3b2cea8b37430c15ec
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-08 08:31:35 +0100

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,12 @@ def add_integer(a, b=98):
         The addition of the two integers.
     Raises:
         TypeError: If a or b are not integers or floats.
+        OverflowError: If a or b are float infinity.
     """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
+    if abs(a) == float("inf") or abs(b) == float("inf"):
+        raise OverflowError("cannot convert float infinity to integer")
     return int(a) + int(b)
