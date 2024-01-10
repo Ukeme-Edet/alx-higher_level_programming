4ccfc85b44aa01820d1ca8e8ba6b7b584e3c523c
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-10 01:28:48 +0100

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function that inserts a line of text to a file, after each line containing a
+specific string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a specific string
+    """
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        lines = f.readlines()
+        i = 0
+        for line in lines:
+            if search_string in line:
+                lines.insert(i + 1, new_string)
+            i += 1
+        f.seek(0)
+        f.writelines(lines)
