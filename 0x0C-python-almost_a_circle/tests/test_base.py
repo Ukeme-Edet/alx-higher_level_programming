20c24b767e62c9567d3303086043d436fd3f05ef
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-15 22:40:01 +0100

    If it's not tested it doesn't work

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for base.py"""
+import unittest
+
+
+class TestBase(unittest.TestCase):
+    """Test cases for base.py"""
+
+    def test_base(self):
+        """Test base.py"""
+        self.assertTrue(True)
