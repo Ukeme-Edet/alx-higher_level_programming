36232937e226bd5d60a8f741b3a9d2f54af30686
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-16 01:05:55 +0100

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,4 +88,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return string representation of Rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
