c14f27ab210f51d565190edfe618105db7ebf955
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-16 01:22:57 +0100

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+"""
+Square class
+- Inherited from Rectangle
+- Private instance attribute: size
+- Getter and setter for size
+- __init__ method
+    - Initialize size using super() with width and height
+- __str__ method
+"""
+
+
+class Square(Rectangle):
+    """The Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize Square class"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return string representation of Square"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Unittest for models/square.py"""
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """Tests for Square class"""
+
+    def setUp(self):
+        """Set up for all methods"""
+        Square._Base__nb_objects = 0
+
+    def test_id(self):
+        """Test id attribute"""
+        s1 = Square(10)
+        self.assertEqual(s1.id, 70)
+
+    def test_size(self):
+        """Test size attribute"""
+        s1 = Square(10)
+        self.assertEqual(s1.size, 10)
+        s2 = Square(2)
+        self.assertEqual(s2.size, 2)
+        s3 = Square(10, 0, 0, 12)
+        self.assertEqual(s3.size, 10)
+        s4 = Square(10, 0, 0, 0)
+        self.assertEqual(s4.size, 10)
+
+    def test_x(self):
+        """Test x attribute"""
+        s1 = Square(10)
+        self.assertEqual(s1.x, 0)
+        s2 = Square(2, 3)
+        self.assertEqual(s2.x, 3)
+        s3 = Square(10, 0, 0, 12)
+        self.assertEqual(s3.x, 0)
+        s4 = Square(10, 3, 0, 0)
+        self.assertEqual(s4.x, 3)
+
+    def test_y(self):
+        """Test y attribute"""
+        s1 = Square(10)
+        self.assertEqual(s1.y, 0)
+        s2 = Square(2, 3, 4)
+        self.assertEqual(s2.y, 4)
+        s3 = Square(10, 0, 0, 12)
+        self.assertEqual(s3.y, 0)
+        s4 = Square(10, 3, 4, 0)
+        self.assertEqual(s4.y, 4)
+
+    def test_area(self):
+        """Test area method"""
+        s1 = Square(3)
+        self.assertEqual(s1.area(), 9)
+        s2 = Square(2)
+        self.assertEqual(s2.area(), 4)
+        s3 = Square(8, 0, 0, 12)
+        self.assertEqual(s3.area(), 64)
+        s4 = Square(10, 3, 4, 0)
+        self.assertEqual(s4.area(), 100)
+
+    def test_display(self):
+        """Test display method"""
+        s1 = Square(4)
+
+    def test_str(self):
+        """Test __str__ method"""
+        s1 = Square(4, 2, 1, 12)
+        self.assertEqual(s1.__str__(), "[Square] (12) 2/1 - 4")
+        s2 = Square(5, 1, 0, 0)
+        self.assertEqual(s2.__str__(), "[Square] (0) 1/0 - 5")
