0ad12657be47b206b9bcfd03ad30f093821a2278
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-14 05:33:39 +0100

    ull creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- A script that creates a table `second_table` in the database `hbtn_0d_usa`.
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT) IF NOT EXISTS;
+CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score)
