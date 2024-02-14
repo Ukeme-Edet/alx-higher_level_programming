e61e4205d9a546a10e4527bd5d48c4158d5cb996
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-14 19:59:29 +0100

    Average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- A script that computes the average of all records in the table second_table of the database hbtn_0d_0
-SELECT AVG(`value`) AS `average`
-FROM `second_table`;
+SELECT AVG(`score`)
+FROM `Second_table`;
