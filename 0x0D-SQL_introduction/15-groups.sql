48da45fbffd4a7f4e825bef2932020e9f4223316
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-14 05:41:17 +0100

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- A script that lists the number of records with the same score in the table `second_table` of the database `hbtn_0d_0`
+SELECT `score`,
+	COUNT(`score`)
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `score` DESC;
