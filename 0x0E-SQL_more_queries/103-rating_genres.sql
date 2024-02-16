14f87a22ba156abf025b0a99abf958bc4c0ec870
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-16 02:02:25 +0100

    Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,9 @@
--- A script that lists all genres from the database `hbtn_0d_tvshows_rate` and their rating
-SELECT `genres`.`name`,
-	`ratings`.`rating`
-FROM `genres`
-	JOIN `ratings` ON `genres`.`id` = `ratings`.`genre_id`
-ORDER BY `genres`.`name`;
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT `name`,
+	SUM(`rate`) AS `rating`
+FROM `tv_genres` AS g
+	INNER JOIN `tv_show_genres` AS s ON s.`genre_id` = g.`id`
+	INNER JOIN `tv_show_ratings` AS r ON r.`show_id` = s.`show_id`
+GROUP BY `name`
+ORDER BY `rating` DESC;
