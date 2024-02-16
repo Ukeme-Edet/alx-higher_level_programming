5d3a90019c35f434643988d07a1ff93aff9237a9
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-16 01:50:56 +0100

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
--- A script that lists all shows contained in the database `hbtn_0d_tvshows` that have at least one genre linked
-SELECT `tv_shows`.`title`,
-	`tv_show_genres`.`genre_id`
-FROM `tv_shows`
-	JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`tv_show_id`
-	AND `tv_show_genres`.`genre_id` IS NOT NULL
-ORDER BY `tv_shows`.`id`;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`,
+	g.`genre_id`
+FROM `tv_shows` AS s
+	INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`
+ORDER BY s.`title`,
+	g.`genre_id`;
