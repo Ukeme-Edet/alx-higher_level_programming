b70d6cf389ba6fc05a3994ac40a39ef9bbf25e9b
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-16 01:25:39 +0100

    States table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- A script tjat creates the table `unique_id`
-CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUe DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
