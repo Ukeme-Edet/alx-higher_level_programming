02b743d7a9bf62017969e1859c101e0a254c9de2
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-16 01:27:47 +0100

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- A script that creates the database `hbtn_0d_usa` and the table `cities`
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+);
