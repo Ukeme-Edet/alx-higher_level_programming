7f0c8ee8ad8fe33001334c1ecc7e151042643748
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-12 00:14:11 +0100

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,4 +2,4 @@
 function add (a, b) {
   return a + b;
 }
-console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
