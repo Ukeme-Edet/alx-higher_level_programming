43033fb5b229b193e37930ff9ce9bc63914d216c
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-18 00:02:22 +0100

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -23,7 +23,7 @@ def main():
     cur = db.cursor()
     cur.execute(
         "SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
-        states ON cities.state_id = states.id ORDER BY cities.id ASC"
+        states ON states.id = cities.state_id ORDER BY cities.id ASC"
     )
     rows = cur.fetchall()
     for row in rows:
