267635e35e390bc80c8c29dc1420b82fb402151d
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-22 18:25:51 +0100

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""
-Defines a `City` class that inherits from `Base = declarative_base()`
-"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""Definition of the City class"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class City(Base):
-    """
-    A class that represents a `cities` table
-    """
+    """Class City"""
+
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(
+        Integer,
+        autoincrement=True,
+        primary_key=True,
+        nullable=False,
+        unique=True,
+    )
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
