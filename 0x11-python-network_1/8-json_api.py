c4a3fb8957c3a2a775dbc8a0532dcb7e1009f765
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:40:26 +0100

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+from json import JSONDecodeError
+
+
+def main():
+    import requests
+    from sys import argv
+
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+
+    r = requests.post(url, data=payload)
+    try:
+        json = r.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except JSONDecodeError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
