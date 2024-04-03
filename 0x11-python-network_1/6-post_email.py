31dc7175d812309f26f283234769b52c784e42b0
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:32:49 +0100

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,18 +12,17 @@ def main():
     passed URL with the email as a parameter, and displays the body of the
     response (decoded in utf-8)
     """
-    import urllib.parse
-    import urllib.request
+    
+    import requests
     from sys import argv
 
     url = argv[1]
     email = argv[2]
 
-    data = urllib.parse.urlencode({"email": email})
-    data = data.encode("ascii")
+    data = {"email": email}
 
-    with urllib.request.urlopen(url, data) as response:
-        print(response.read().decode("utf-8"))
+    response = requests.post(url, data=data)
+    print(response.text)
 
 
 if __name__ == "__main__":
