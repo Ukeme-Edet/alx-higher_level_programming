2f6da5ea6f4b8b767a636d4e28829fd5c489cb57
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:57:00 +0100

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Python script that takes a Github repository and user credentials (username and
+password) and uses the Github API to display the last 10 commits
+"""
+
+
+def main():
+    """
+    Main function for the script
+    """
+
+    import requests
+    from sys import argv
+
+    username = argv[1]
+    repo = argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
+    r = requests.get(url)
+
+    json = r.json()
+    for commit in json[:10]:
+        commit = commit.get("commit")
+        author = commit.get("author")
+        sha = commit.get("tree").get("sha")
+        print("{}: {}".format(sha, author.get("name")))
+
+
+if __name__ == "__main__":
+    main()
