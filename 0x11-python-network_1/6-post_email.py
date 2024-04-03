4202fdbf0117d479b0ec6ff2b04a463612409a86
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:33:38 +0100

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,7 +12,7 @@ def main():
     passed URL with the email as a parameter, and displays the body of the
     response (decoded in utf-8)
     """
-    
+
     import requests
     from sys import argv
 
