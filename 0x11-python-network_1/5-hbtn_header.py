6c0ba7ae614773ff75ac40505bac410a50faa17a
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:27:46 +0100

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response.
+"""
+
+
+def main():
+    """
+    This function takes in a URL, sends a request to the URL and displays the
+    value of the X-Request-Id variable found in the header of the response.
+    """
+    import requests
+    from sys import argv
+
+    response = requests.get(argv[1])
+    print(response.headers.get("X-Request-Id"))
+
+
+if __name__ == "__main__":
+    main()
