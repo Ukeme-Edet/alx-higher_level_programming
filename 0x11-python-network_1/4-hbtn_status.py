af21fab11b0be03b246058ca76d855a6e47fd7b1
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:24:02 +0100

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 
 def main():
     """
-    This function fetches https://intranet.hbtn.io/status
+    This function fetches https://alx-intranet.hbtn.io/status
     """
     import requests
 
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
