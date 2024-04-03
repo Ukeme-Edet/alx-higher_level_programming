134cda567f0738c48c554e2b5797627c1b6474a1
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-04 00:20:27 +0100

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+"""
+
+
+def main():
+    """
+    This function takes in a URL, sends a request to the URL and displays the
+    body of the response (decoded in utf-8).
+    """
+    import urllib.request
+    from urllib.error import HTTPError
+    from sys import argv
+
+    url = argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    main()
