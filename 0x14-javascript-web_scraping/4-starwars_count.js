81287cd593499f6fd3e9ac2327dd33af57738408
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-17 14:34:03 +0100

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-request(url, function (error, _, body) {
+request(process.argv[2], function (error, _, body) {
   if (error) {
     console.log(error);
   } else {
     let count = 0;
-    const films = JSON.parse(body).results;
-    for (const film of films) {
+    for (const film of JSON.parse(body).results) {
       for (const character of film.characters) {
         if (character.includes('18')) {
           count++;
