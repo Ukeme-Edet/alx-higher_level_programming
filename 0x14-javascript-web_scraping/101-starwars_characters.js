da2e060ba5923dd916ae48fdd606121b51be9724
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-17 15:05:08 +0100

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,43 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, _, body) {
+
+request(url, async (error, _, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, _, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
+    const charDict = {};
+
+    const fetchCharacter = async (character) => {
+      try {
+        const response = await new Promise((resolve, reject) => {
+          request(character, (error, _, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(body);
+            }
+          });
+        });
+        const { url, name } = JSON.parse(response);
+        charDict[url] = name;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const fetchAllCharacters = async () => {
+      try {
+        await Promise.all(characters.map(fetchCharacter));
+        for (const key of characters) {
+          console.log(charDict[key]);
         }
-      });
-    }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    await fetchAllCharacters();
   }
 });
