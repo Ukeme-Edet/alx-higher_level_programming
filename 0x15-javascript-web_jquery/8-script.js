135143e8d9b7d5266ef0f996e917502edd30677c
Ukeme Edet <ukemeedet2207@gmail.com>
2024-08-13 03:00:07 +0100

    feat: Sanitize film titles before appending to the list

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   const url = 'https://swapia-api.alx-tools.com/api/films/?format=json';
   $.get(url, function (data, textStatus) {
     for (const result of data.results) {
-      $('UL#list_movies').append(`<li>${result.title}</li>`);
+      const sanitizedTitle = $('<div>').text(result.title).html();
+      $('UL#list_movies').append(`<li>${sanitizedTitle}</li>`);
     }
   });
 });
