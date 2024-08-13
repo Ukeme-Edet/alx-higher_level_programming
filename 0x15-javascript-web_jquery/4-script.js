e39f17f930509ec6e83eea4d5ca2030d101698d1
Ukeme Edet <ukemeedet2207@gmail.com>
2024-08-13 02:46:21 +0100

    feat: Refactor header color toggle logic in JavaScript

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   $('div#update_header').click(function () {
-    $('header').hasClass('green')
-      ? $('header').removeClass('green') && $('header').addClass('red')
-      : $('header').removeClass('red') && $('header').addClass('green');
+    $('header').toggleClass('red green');
   });
 });
